289ebfd076d91d4eda3958281322f59b7cf404e8
Greg Bowler <greg.bowler@g105b.com>
2021-03-26 16:13:24 +0000

    docs: add inbuilt server example

--- example/07-greeter.php
+++ example/07-greeter.php
@@ -1,13 +1,19 @@
 <!doctype html>
 <meta charset="utf-8" />
 <title>Greeter!</title>
-<h1>
-	Hello, <?php
+
+<h1>Hello, <?php
 $requested = trim($_SERVER["REQUEST_URI"], "/");
 $name = $requested ?: "World";
 echo "$name!";
-?>
-</h1>
+?></h1>
 <p>This is just an example HTML page to interactively greet the user.</p>
 <p>To use it, run 08-php-inbuilt-server.php - it will spawn a new server process and use THIS script as its router.</p>
+<p>Being a router, <a href="/Example">all requests</a> are routed through it.</p>
 <p>Have fun!</p>
+<?php
+// Log the request, so it shows up in the 08-php-inbuilt-server.php logs:
+$stdout = fopen("php://stdout", "w");
+$responseCode = http_response_code();
+$logMsg = "[$responseCode] $_SERVER[REQUEST_METHOD] /$requested";
+fwrite($stdout, $logMsg . PHP_EOL);
--- example/08-php-inbuilt-server.php
+++ example/08-php-inbuilt-server.php
@@ -4,4 +4,38 @@ use Gt\Daemon\Process;
 require_once(__DIR__ . "/../vendor/autoload.php");
 
 $serverProcess = new Process("php", "-S", "0.0.0.0:8080", "07-greeter.php");
-// TODO!
+$serverProcess->exec();
+if(!$serverProcess->isRunning()) {
+	echo "Server is not running...", PHP_EOL;
+	exit;
+}
+
+echo "Executing cURL...", PHP_EOL;
+$curlCody = new Process("curl", "http://localhost:8080/Cody");
+$curlCody->exec();
+
+$curlScarlett = new Process("curl", "http://localhost:8080/Scarlett");
+$curlScarlett->exec();
+
+
+while($curlCody->isRunning() || $curlScarlett->isRunning()) {
+	sleep(1);
+}
+
+usleep(1_000);
+echo "Response from Cody's greeter: ", PHP_EOL, $curlCody->getOutput(), PHP_EOL;
+echo "Response from Scarlett's greeter: ", PHP_EOL, $curlScarlett->getOutput(), PHP_EOL;
+
+echo "This script will stay open. Try hitting the site yourself to see the server logs: http://localhost:8080", PHP_EOL, PHP_EOL;
+
+while($serverProcess->isRunning()) {
+	if($output = $serverProcess->getOutput()) {
+		foreach(explode("\n", $output) as $line) {
+			if(empty(trim($line))) {
+				continue;
+			}
+			echo "[Server]: $line", PHP_EOL;
+		}
+	}
+	usleep(1_000);
+}
